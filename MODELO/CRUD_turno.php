8e9a5bd46351780e9175ff7d4ebe18828a1a46c2
Janina Samudio <samudiojanina@gmail.com>
2024-12-07 14:46:09 -0300

    Agregado del archivo Validación en MODELO

--- MODELO/CRUD_turno.php
+++ MODELO/CRUD_turno.php
@@ -2,5 +2,10 @@
     require "../CONTROLADOR/DataBase/conexion.php";
 
     if (isset($_POST['iniciarS'])) {
+        $ci = $_POST['cedula'];
+        $nombre = $_POST['nomAp'];
+        $correo = $_POST['correo'];
+        $turno = $_POST['turno'];
+
         header('location: ../VISTA/ReservaTurno.php ');
     }
---
+++ MODELO/Validación.php
@@ -0,0 +1,10 @@
+<?php
+    require "../CONTROLADOR/DataBase/conexion.php";
+
+    if (isset($_POST['iniciarS'])) {
+        $usuario = $_POST['usuario'];
+        $clave = $_POST['clave'];
+    } else {
+        # code...
+    }
+    
